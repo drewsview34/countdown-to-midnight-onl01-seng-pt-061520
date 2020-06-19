126c4cd55e655a5dc743f4fbd9386386135fe05b
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-19 15:24:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,12 @@
 def countdown(num)
   while num > 0
     puts "#{num} SECOND(S)!"
-    sleep(5.seconds)
+    countdown_with_sleep(5)
     num -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num_sec)
-  while num_sec
+
 end 
